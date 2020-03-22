63de8af2c6ea29de2ac0b8243d442f098d84da93
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2020-03-22 01:01:28 -0500

    update in case I break everything

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            /* Split the screen in half */
+            .split {
+                height: 100%;
+                width: 50%;
+                position: fixed;
+                z-index: 1;
+                top: 0;
+                overflow-x: hidden;
+                padding-top: 20px;
+            }
+
+            /* Control the left side */
+            .left {
+                left: 0;
+                background-image: url("woof.jpg");
+            }
+
+            /* Control the right side */
+            .right {
+                right: 0;
+                background-color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Wuphf
+                </div>
+                <div class="split left">
+                    <div class="centered">
+                        <img src="img_avatar2.png" alt="Avatar woman">
+                        <h2>Jane Flex</h2>
+                        <p>Some text.</p>
+                    </div>
+                </div>
+
+                <div class="split right">
+                    <div class="centered">
+                        <img src="img_avatar.png" alt="Avatar man">
+                        <h2>John Doe</h2>
+                        <p>Some text here too.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
