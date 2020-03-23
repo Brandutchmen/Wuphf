fe3d95a310e630192796807b72c6007458554e62
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2020-03-22 20:54:34 -0500

    Vue thing

---
+++ assets/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn
+        icon
+        @click.native.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+<!--      <v-btn-->
+<!--        icon-->
+<!--        @click.native.stop="clipped = !clipped"-->
+<!--      >-->
+<!--        <v-icon>web</v-icon>-->
+<!--      </v-btn>-->
+<!--      <v-btn-->
+<!--        icon-->
+<!--        @click.native.stop="fixed = !fixed"-->
+<!--      >-->
+<!--        <v-icon>remove</v-icon>-->
+<!--      </v-btn>-->
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <v-spacer/>
+        <span>Made with ❤ by Brandon Young </span>
+      <v-spacer/>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import Meta from 'mixins/meta'
+
+  export default {
+    mixins: [Meta],
+
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Wuphf'
+      }
+    }
+  }
+</script>
